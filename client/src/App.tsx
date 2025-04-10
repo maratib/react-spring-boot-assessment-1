a03277ba0d08dd373c2b497cff53744d0ca90a65
waleedalikhan <waleedomore@gmail.com>
2025-04-11 02:35:22 +0500

    assessment updates

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,33 @@
+import { AppSidebar } from "@/components/custom/AppSidebar";
 import { ThemeButton } from "@/components/custom/ThemeButton";
-import { ListView } from "@/components/views/list-view";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { ListView } from "@/components/views/ListView";
+import { TableSkeleton } from "@/components/views/TableSkeleton";
+import { Suspense } from "react";
 
 function App() {
   return (
-    <>
-      {/* Navbar  */}
-      <nav className="flex items-center justify-between px-6 py-4 shadow-md bg-[#374151] text-white dark:bg-[#374151]">
-        {/* Logo */}
-        <div className="text-xl font-bold">LOGO</div>
+    <SidebarProvider>
+      <AppSidebar />
+      <main className="flex-1 overflow-x-hidden">
+        {/* Navbar  */}
+        <nav className="flex items-center justify-between px-6 py-4 shadow-md bg-[#374151] text-white dark:bg-[#374151]">
+          <div className="flex items-center gap-2">
+            <SidebarTrigger />
+            {/* Logo */}
+            <div className="text-xl font-bold">LOGO</div>
+          </div>
 
-        {/* ThemeToggleButton */}
-        <ThemeButton />
-      </nav>
-      <div className="flex flex-col items-center justify-center min-h-svh">
-        <ListView />
-      </div>
-    </>
+          {/* ThemeToggleButton */}
+          <ThemeButton />
+        </nav>
+        <div className="p-10">
+          <Suspense fallback={<TableSkeleton />}>
+            <ListView />
+          </Suspense>
+        </div>
+      </main>
+    </SidebarProvider>
   );
 }
 
